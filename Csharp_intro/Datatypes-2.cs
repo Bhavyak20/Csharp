3f418263362c094e2dd1cb4b9c130700b41adc06
Bhavyak20 <bhavyak.fullstack@gmail.com>
2025-06-16 22:25:03 +0530

    Practice of Csharp for June16th(Datatypes)

---
+++ Csharp_intro/Datatypes-2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Security.Cryptography;
+class Datatypes_2
+    {
+    static void Main()
+    {
+        //Display the max and min values of numeric datatypes
+
+        Console.WriteLine("byte Max Value " + byte.MaxValue);
+        Console.WriteLine("byte Min Value " + byte.MinValue);
+        Console.WriteLine("short maximum value " + short.MaxValue);
+
+
+
+        //${________________________________________________}
+        //String interpolation
+        // Display the maximum and minimum values using string interpolation
+        Console.WriteLine($"byte datatype MaxValue: {byte.MaxValue} and MinValue: {byte.MinValue}");
+        Console.WriteLine($"short minimum value : {byte.MinValue} and maximum value is: {short.MaxValue}");
+        Console.WriteLine($"short datatype MaxValue: {short.MaxValue} and MinValue: {short.MinValue}");
+        Console.WriteLine($"ushort datatype MaxValue: {ushort.MaxValue} and MinValue: {ushort.MinValue}");
+        Console.WriteLine($"uint datatype MaxValue: {uint.MaxValue} and MinValue: {uint.MinValue}");
+        Console.WriteLine($"ulong datatype MaxValue: {ulong.MaxValue} and MinValue: {ulong.MinValue}");
+        Console.WriteLine($"float datatype MaxValue: {float.MaxValue} and MinValue: {float.MinValue}"); // 3.402823E+38 means 3.402823*10^38 (10 to the power of 38)
+        Console.WriteLine($"decimal datatype MaxValue: {decimal.MaxValue} and MinValue: {decimal.MinValue}");
+        Console.WriteLine($"double datatype MaxValue: {double.MaxValue} and MinValue: {double.MinValue}"); // double has a greater range than int
+        Console.WriteLine($"int datatype MaxValue: {int.MaxValue} and MinValue: {int.MinValue}");
+        Console.WriteLine($"long datatype MaxValue: {long.MaxValue} and MinValue: {long.MinValue}");
+
+
+        Console.WriteLine($"decimal datatype MaxValue: {decimal.MaxValue} and MinValue: {decimal.MinValue}");
+        Console.WriteLine($"flaot datatype MaxValue: {float.MaxValue} and MinValue: {float.MinValue}");
+        Console.WriteLine($"double datatype MaxValue: {double.MaxValue} and MinValue: {double.MinValue}");
+
+        Main2();
+
+    }
+    static void Main2() {
+        //bool data type
+        bool sevenwonders = true;
+        bool sixwonders = false;
+        Console.WriteLine("we have total seven wonders " + sevenwonders);
+        Console.WriteLine("we have total six wonders " + sixwonders);
+
+        //char data type
+
+        char initial = 'K';
+        Console.WriteLine($"My surname is {initial} ");
+
+        //string data type
+
+        string myname = "Bhavya sri" ;
+        Console.WriteLine($"My name is {myname}");
+    }
+
+}
+
---
+++ Csharp_intro/Datatypes.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.CodeDom;
+class Datatypes
+    {
+    static void Main()
+    {
+        //I'm using datatypes in this class 
+        //1.Numeric data types in numeric DT there are two main types non-floating and floating
+        //under nonfloating 1.byte 2.short 3.ushort 4.int 5.unit 6.long 7.ulong
+
+        Console.WriteLine("****Now I'm writing byte data types****");
+        //range of byte is 0 to 255 it won't take value more than 255
+        byte age = 2;
+        byte grade = 4;
+        Console.WriteLine("My age is " +age);
+        Console.WriteLine("I'm studying " +grade);
+
+        Console.WriteLine("***Now example for short***");
+        //Range for short is -32768 to 32767;
+        short temparature = -20;
+        Console.WriteLine("Today's temparature is " + temparature);
+
+        //short value1 = -33333 //-- > op:it will show error;
+        //Console.WriteLine(value1);
+
+        Console.WriteLine("*** writing ushort example ***");
+        //Range of ushort is 0 to 65,535
+        ushort population = 60000;
+        Console.WriteLine("Population in Vijayawada is " + population);
+
+        Console.WriteLine("*** writing example for int ***");
+        //Range of int is -2,147,483,648 to 2,147,483,648
+
+        int world = 2045670120;
+        Console.WriteLine("Total world population is " +world);
+
+        Console.WriteLine("*** writing example for unit ***");
+        //unit range is 0 to 4,294,967,295
+
+        uint stars = 4000520199;
+        Console.WriteLine("Maximum stars in galaxy will be more than " + stars);
+
+        /*
+        * long
+            Range: -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+            Purpose: Used for very large or small integers
+        */
+        long worldPopulation = 7800000000L;
+        Console.WriteLine("Long Value:" + worldPopulation);
+        long point = -9;
+        Console.WriteLine(point);
+
+        /*ulong
+            Range: 0 to 18,446,744,073,709,551,615
+            Purpose: Large positive integers
+        */
+
+        ulong bankBalance = 100000000000000UL;
+        Console.WriteLine("Ulong Value:" + bankBalance);
+
+
+        /*
+              Floating - Point Types(Fractional Values Allowed)
+              decimal : ±79,228,162,514,264,337,593,543,950,335
+              float   : ±3.40282347E+38F
+              double  : ±1.7976931348623157E+308
+       */
+        decimal price = 199.99m;
+        Console.WriteLine("Price Value:" + price);
+
+        float pi = 3.1415199999f;
+        Console.WriteLine("Pi Value:" + pi);
+
+        double stockPrice = 987.65d;
+        Console.WriteLine("stockPrice Value:" + stockPrice);
+
+
+
+    }
+}
+
