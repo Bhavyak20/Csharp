0015a0f89815e6ac5c62a903ad0ff557801b8d32
Bhavyak20 <bhavyak.fullstack@gmail.com>
2025-06-16 09:17:12 +0530

    Add project files.

---
+++ Csharp_intro/1_Basics.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+     class FirstProgram
+    {
+        static void Main()
+        {
+        Console.WriteLine("My Method in Program Class");
+        Console.WriteLine("My first code in C sharp");
+        //Console.ReadLine();
+        string value = Console.ReadLine();
+        Console.WriteLine("My name is:" +value);
+
+        
+        displaymessage();
+        Readmessages();
+        }
+
+
+    static void displaymessage()
+    {
+        Console.WriteLine("---->This is second method<----");
+
+        Console.WriteLine("What is your first name ?");
+        string firstname= Console.ReadLine();
+
+        Console.WriteLine("What is your last name ?");
+        string lastname= Console.ReadLine();
+
+        string fullname = firstname + lastname;
+        Console.WriteLine("Your full name is:" + fullname);
+    }
+    static void Readmessages()
+    {
+        Console.WriteLine("--->This is third method name is Read message<---");
+    }
+    }
+
