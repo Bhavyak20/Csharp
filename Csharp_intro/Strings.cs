2e0d8ee08317ef597f09f7dcd34b5c63e0408d73
Bhavyak20 <bhavyak.fullstack@gmail.com>
2025-06-17 17:07:25 +0530

    Practice of June 17th

---
+++ Csharp_intro/Strings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+ class Strings
+    {
+    static void Main()
+    {
+        Console.WriteLine("*** Concept is strings ***");
+        string firstname = "Bhavya";
+        string lastname = "sri";
+        string fullname = firstname + lastname;
+        //string fullname = ($"My first name is {firstname} and last name is {lastname}");
+        Console.WriteLine(fullname);
+
+        string firstcompany = "\"DXC \"";
+        Console.WriteLine("I worked in " + firstcompany);
+
+        //to assign a path from our sytem
+
+        string coursepath = "C\\Users\\Kolli\\Desktop\\ConsoleApplication\\Csharp";
+        Console.WriteLine($"My course path is {coursepath}");
+
+        //to print everything in new line we use \n
+
+        string numbers = " \n One\n TWO \n Three";
+        Console.WriteLine("Numbers we are using here is " + numbers);
+
+        //using string as questionble
+        Console.WriteLine("what is your name ?" );
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your surname ?");
+        string surname = Console.ReadLine();
+
+        string realname = name + surname;
+        Console.WriteLine("My full name is " + realname);
+
+        //interpolation type
+
+        Console.WriteLine($"My firstname is {firstname} and last name is {lastname}. So my full name is {fullname}");
+       
+
+    }
+    }
+
---
+++ Csharp_intro/VarAndDynamic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+internal class VarAndDynamic
+    {
+    static void Main()
+    {
+        Console.WriteLine("*** Now I'm writing examples for Var and dynamic ***");
+        //var keyword examples
+        //for var keyword we can assign differnt value but it has to be same datatype which we mentioned at intial
+        var v_myname = "Bhavya";
+        v_myname = "Divya";
+        v_myname = "Kavya";
+        Console.WriteLine("My name is " + v_myname);
+        Console.WriteLine("Data type of v_myname is " + v_myname.GetType());
+
+        var v_number = 12345;
+        v_number = 6789;
+        Console.WriteLine("My number is " +v_number);
+        Console.WriteLine($"Data type of v_number is {v_number.GetType()}");
+
+        var v_temparature = 3.2f;
+        Console.WriteLine("Today temparature is " + v_temparature);
+        Console.WriteLine("Data TYpe of V_temparature is " + v_temparature.GetType());
+
+        dynamic();
+    }
+
+    static void dynamic() {
+        Console.WriteLine("*** DYNAMIC Key word example ***");
+        //dynamic keyword examples
+        //for dynamic keyword we can assign differnt value and can alos different datatype 
+        dynamic d_number = 9;
+        d_number = "sree";
+        Console.WriteLine("Number given in in dynamic method is " + d_number);
+        Console.WriteLine("Data type of d_number is " + d_number.GetType());
+
+        dynamic d_price = 600000123;
+        d_price = 99.6m;
+        d_price = -65.9;
+        Console.WriteLine("Price value is " + d_price);
+        Console.WriteLine("Data type of d_price is " + d_price.GetType());
+    }
+    }
+
